06e27b5e1139d4eabb50b55621a6163bc024104b
Oriel Shmuel <Orielshmuel@gmail.com>
2025-01-05 12:50:19 +0200

    Update common.ts

--- client/src/config/common.ts
+++ client/src/config/common.ts
@@ -1,2 +1,2 @@
 export const MIN_USER_AGE = 16;
-export const baseURL = "https://gym-4m0bzqodn-orielshs-projects.vercel.app/api";
+export const baseURL = "https://gym-orielshs-projects.vercel.app/api";
