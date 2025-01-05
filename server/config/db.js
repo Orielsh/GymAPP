4811024f5468f1d555245447c05253b0be1c389a
Oriel Shmuel <Orielshmuel@gmail.com>
2025-01-05 11:49:00 +0200

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const mode = process.env.NODE_ENV;  // 'prod' or 'dev'
 let uri;
 
 if (mode === 'prod') {
-  uri = process.env.MONGODB_URI_PROD
+  uri = process.env.MONGODB_URI
   console.log('Environment set to PRODUCTION mode')
 } else {
   uri = process.env.MONGODB_URI_DEV
